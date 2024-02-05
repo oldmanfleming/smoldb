0403c872d4b55fc009413c765358d1ea06ac2e8f
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-02-05 15:04:03 -0800

    Add initial cli interface and kv lib

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![deny(missing_docs)]
+//! A simple key/value store
+
+mod kv_store;
+
+pub use kv_store::KvStore;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    #[command(name = "get")]
+    Get(GetCommand),
+    #[command(name = "set")]
+    Set(SetCommand),
+    #[command(name = "rm")]
+    Remove(RemoveCommand),
+}
+
+#[derive(Args, Debug)]
+struct GetCommand {
+    key: String,
+}
+
+#[derive(Args, Debug)]
+struct SetCommand {
+    key: String,
+    value: String,
+}
+
+#[derive(Args, Debug)]
+struct RemoveCommand {
+    key: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Get(GetCommand { .. }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Command::Set(SetCommand { .. }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Command::Remove(RemoveCommand { .. }) => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+    }
 }
