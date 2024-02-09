c2335717653fb9663d2e6ac3d3255a686b2da210
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-02-09 12:54:24 -0800

    Switch from json serde to bitcask format

--- src/store/error.rs
+++ src/store/error.rs
@@ -7,13 +7,17 @@ pub enum KvsError {
     #[error("An IO error occurred: {0}")]
     Io(#[from] std::io::Error),
 
-    /// Serialization or deserialization error.
-    #[error("A serialization or deserialization error occurred: {0}")]
-    Serde(#[from] serde_json::Error),
+    /// SystemTime error.
+    #[error("A system time error occurred: {0}")]
+    SystemTime(#[from] std::time::SystemTimeError),
 
     /// Key not found error.
     #[error("Key not found")]
     KeyNotFound,
+
+    /// Data corruption error.
+    #[error("Data corruption error: {0}")]
+    DataCorruption(String),
 }
 
 /// The `Result` type for `KvStore`.
--- src/store/kv_store.rs
+++ src/store/kv_store.rs
@@ -1,12 +1,24 @@
-use serde::{Deserialize, Serialize};
+use crate::{KvsError, Result};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::{
     collections::HashMap,
     fs,
-    io::{BufRead, Write},
+    io::{Read, Seek, Write},
     path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
 };
 
-use crate::{KvsError, Result};
+const X25: crc::Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_IBM_SDLC);
+
+const TOMBSTONE: &[u8; 0] = b"";
+
+#[derive(Debug, Clone)]
+struct Entry {
+    file_id: String,
+    value_len: u32,
+    value_pos: u64,
+    timestamp: u64,
+}
 
 /// The `KvStore` stores string key/value pairs.
 ///
@@ -23,7 +35,7 @@ use crate::{KvsError, Result};
 /// ```
 #[derive(Default)]
 pub struct KvStore {
-    store: HashMap<String, String>,
+    key_dir: HashMap<Vec<u8>, Entry>,
     path_buf: PathBuf,
 }
 
@@ -35,21 +47,34 @@ impl KvStore {
         let mut path_buf: PathBuf = path.into();
         path_buf.push("db");
 
-        Ok(KvStore {
-            store: HashMap::new(),
+        let mut store = KvStore {
+            key_dir: HashMap::new(),
             path_buf,
-        })
+        };
+
+        store.load_store()?;
+
+        Ok(store)
     }
 
     /// Sets the value of a string key to a string.
     ///
     /// If the key already exists, the previous value will be overwritten.
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        let file = self.get_file()?;
+        if let Some(entry) = self.key_dir.get(key.as_bytes()).cloned() {
+            if entry.value_len == 0 {
+                return Ok(None);
+            }
 
-        self.load_store(&file)?;
-
-        Ok(self.store.get(&key).cloned())
+            let file = self.get_file()?;
+            let mut reader = std::io::BufReader::new(file);
+            let value_bytes = self.read_value(&mut reader, &entry)?;
+            let value = String::from_utf8(value_bytes).map_err(|err| {
+                KvsError::DataCorruption(format!("Invalid UTF-8 sequence: {}", err))
+            })?;
+            return Ok(Some(value));
+        }
+        Ok(None)
     }
 
     /// Gets the string value of a given string key.
@@ -57,8 +82,9 @@ impl KvStore {
     /// Returns `None` if the given key does not exist.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let file = self.get_file()?;
-
-        self.write_command(file, Command::Set { key, value })?;
+        let mut writer = std::io::BufWriter::new(file);
+        let entry = self.write_entry(&mut writer, key.as_bytes(), value.as_bytes())?;
+        self.key_dir.insert(key.into(), entry);
 
         Ok(())
     }
@@ -67,57 +93,134 @@ impl KvStore {
     ///
     /// Returns `KvsError::KeyNotFound` if the key does not exist.
     pub fn remove(&mut self, key: String) -> Result<()> {
-        let file = self.get_file()?;
-
-        self.load_store(&file)?;
-
-        if self.store.get(&key).is_none() {
+        let key_bytes = key.as_bytes();
+        if self.key_dir.get(key_bytes).is_none() {
             return Err(KvsError::KeyNotFound);
         }
-
-        self.write_command(file, Command::Remove { key })?;
-
+        let file = self.get_file()?;
+        let mut writer = std::io::BufWriter::new(file);
+        let entry = self.write_entry(&mut writer, key_bytes, TOMBSTONE)?;
+        self.key_dir.insert(key_bytes.to_vec(), entry);
         Ok(())
     }
 
     fn get_file(&mut self) -> Result<std::fs::File> {
         Ok(fs::OpenOptions::new()
             .read(true)
-            .write(true)
             .create(true)
             .append(true)
             .open(&self.path_buf)?)
     }
 
-    fn load_store(&mut self, file: &std::fs::File) -> Result<()> {
-        let reader = std::io::BufReader::new(file);
-        let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
-
-        while let Some(command) = stream.next() {
-            match command? {
-                Command::Set { key, value } => {
-                    self.store.insert(key, value);
-                }
-                Command::Remove { key } => {
-                    self.store.remove(&key);
-                }
-            }
+    fn load_store(&mut self) -> Result<()> {
+        let file = self.get_file()?;
+        let mut reader = std::io::BufReader::new(file);
+        while let Some((key, entry)) = self.read_entry(&mut reader)? {
+            self.key_dir.insert(key, entry);
         }
-
         Ok(())
     }
 
-    fn write_command(&mut self, file: std::fs::File, command: Command) -> Result<()> {
-        let mut writer = std::io::BufWriter::new(file);
-        serde_json::to_writer(&mut writer, &command)?;
+    // Write a key/value pair to the given writer in the bitcask format.
+    // Fixed-width header Variable-length body
+    //+=====+=====+=====+====== - - +============== - - +
+    //| u16 | u64 | u32 | u32       | [u8] | [u8] |
+    //+=====+=====+=====+====== - - +============== - - +
+    // checksum (2 bytes)
+    // timestamp (8 bytes)
+    // key_len (4 bytes)
+    // val_len (4 bytes)
+    // key (key_len bytes)
+    // value (val_len bytes)
+    fn write_entry<W: Write + Seek>(
+        &mut self,
+        writer: &mut W,
+        key: &[u8],
+        value: &[u8],
+    ) -> Result<Entry> {
+        let key_len = key.len();
+        let value_len = value.len();
+        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+        let mut entry = Vec::<u8>::with_capacity(8 + 4 + 4 + key_len + value_len);
+
+        entry.write_u64::<BigEndian>(timestamp)?;
+        entry.write_u32::<BigEndian>(key_len as u32)?;
+        entry.write_u32::<BigEndian>(value_len as u32)?;
+        entry.write_all(key)?;
+        entry.write_all(value)?;
+
+        let checksum = X25.checksum(&entry);
+
+        writer.write_u16::<BigEndian>(checksum)?;
+
+        writer.write_all(&entry)?;
+
         writer.flush()?;
 
-        Ok(())
-    }
-}
+        let value_pos = writer.seek(std::io::SeekFrom::Current(0))? - value_len as u64;
 
-#[derive(Debug, Serialize, Deserialize)]
-enum Command {
-    Set { key: String, value: String },
-    Remove { key: String },
+        Ok(Entry {
+            file_id: String::new(),
+            value_len: value_len as u32,
+            value_pos,
+            timestamp,
+        })
+    }
+
+    fn read_entry<R: Read + Seek>(&self, reader: &mut R) -> Result<Option<(Vec<u8>, Entry)>> {
+        // Check if we are at the end of the reader
+        let current_pos = reader.seek(std::io::SeekFrom::Current(0))?;
+        if current_pos == reader.seek(std::io::SeekFrom::End(0))? {
+            return Ok(None);
+        }
+        reader.seek(std::io::SeekFrom::Start(current_pos))?;
+
+        let checksum = reader.read_u16::<BigEndian>()?;
+        let timestamp = reader.read_u64::<BigEndian>()?;
+        let key_len = reader.read_u32::<BigEndian>()?;
+        let value_len = reader.read_u32::<BigEndian>()?;
+
+        let mut key = vec![0; key_len as usize];
+        reader.read_exact(&mut key)?;
+
+        let value_pos = reader.seek(std::io::SeekFrom::Current(0))?;
+
+        let mut value = vec![0; value_len as usize];
+        reader.read_exact(&mut value)?;
+
+        let mut entry_bytes =
+            Vec::<u8>::with_capacity(8 + 4 + 4 + key_len as usize + value_len as usize);
+        entry_bytes.write_u64::<BigEndian>(timestamp)?;
+        entry_bytes.write_u32::<BigEndian>(key_len)?;
+        entry_bytes.write_u32::<BigEndian>(value_len)?;
+        entry_bytes.write_all(&key)?;
+        entry_bytes.write_all(&value)?;
+
+        let read_checksum = X25.checksum(&entry_bytes);
+
+        if checksum != read_checksum {
+            return Err(KvsError::DataCorruption(format!(
+                "Checksum mismatch: expected {}, got {}",
+                checksum, read_checksum
+            )));
+        }
+
+        let entry = Entry {
+            file_id: String::new(),
+            value_len,
+            value_pos,
+            timestamp,
+        };
+
+        Ok(Some((key, entry)))
+    }
+
+    fn read_value<R: Read + Seek>(&self, reader: &mut R, entry: &Entry) -> Result<Vec<u8>> {
+        reader.seek(std::io::SeekFrom::Start(entry.value_pos))?;
+
+        let mut value = vec![0; entry.value_len as usize];
+        reader.read_exact(&mut value)?;
+
+        Ok(value)
+    }
 }
