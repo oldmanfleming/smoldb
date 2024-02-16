4e066914c799c6a301443aa6180c62b67f76f6e3
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-02-16 08:11:02 -0800

    Create new active file when a size threshold is reached

--- src/store/kv_store.rs
+++ src/store/kv_store.rs
@@ -14,6 +14,8 @@ const TOMBSTONE: &str = "";
 
 const LOG_FILE: &str = "log";
 
+const LOG_SIZE_THRESHOLD: u64 = 1024 * 1024;
+
 /// The `KvStore` stores string key/value pairs.
 ///
 /// Key/value pairs are stored on disk using the Bitcask append-only log format.
@@ -32,7 +34,7 @@ pub struct KvStore {
     path: PathBuf,
     writer: BufWriter<File>,
     readers: HashMap<u64, BufReader<File>>,
-    curr_file_id: u64,
+    active_file_id: u64,
 }
 
 #[derive(Debug, Clone)]
@@ -66,7 +68,7 @@ impl KvStore {
 
         // get the last file id or 0 if there are no files
         // the last file is the current file that we write too
-        let curr_file_id = match file_ids.last() {
+        let active_file_id = match file_ids.last() {
             Some(&id) => id,
             None => {
                 file_ids.push(0);
@@ -77,7 +79,7 @@ impl KvStore {
             fs::OpenOptions::new()
                 .create(true)
                 .append(true)
-                .open(log_path(&path, curr_file_id))?,
+                .open(log_path(&path, active_file_id))?,
         );
 
         // open a reader for each file and load the key_dir with it's entries
@@ -90,22 +92,20 @@ impl KvStore {
                     .open(log_path(&path, file_id))?,
             );
 
-            while let Some((key, entry)) = read_next_entry(&mut reader)? {
+            while let Some((key, entry)) = KvStore::read_next_entry(&mut reader, file_id)? {
                 key_dir.insert(key, entry);
             }
 
             readers.insert(file_id, reader);
         }
 
-        let store = KvStore {
+        Ok(KvStore {
             key_dir,
             path,
             writer,
             readers,
-            curr_file_id,
-        };
-
-        Ok(store)
+            active_file_id,
+        })
     }
 
     /// Gets the string value of a given string key.
@@ -117,9 +117,7 @@ impl KvStore {
                 return Ok(None);
             }
 
-            if let Some(reader) = self.readers.get_mut(&entry.file_id) {
-                return Ok(Some(read_value(reader, &entry)?));
-            }
+            return self.read_value(&entry);
         }
         Ok(None)
     }
@@ -128,8 +126,29 @@ impl KvStore {
     ///
     /// If the key already exists, the previous value will be overwritten.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        let entry = write_entry(self.writer.get_mut(), &key, &value)?;
+        let entry = self.write_entry(&key, &value)?;
+
+        // If the size of the active file is greater than the threshold we will create a new active file
+        //
+        // Adding the pos of the last value written to the end of the file with it's length will
+        // give us the total size in bytes of the active file.
+        if entry.value_pos + (entry.value_len as u64) > LOG_SIZE_THRESHOLD {
+            self.active_file_id += 1;
+            let active_file = log_path(&self.path, self.active_file_id);
+            self.writer = BufWriter::new(
+                fs::OpenOptions::new()
+                    .create(true)
+                    .append(true)
+                    .open(&active_file)?,
+            );
+            self.readers.insert(
+                self.active_file_id,
+                BufReader::new(fs::File::open(&active_file)?),
+            );
+        }
+
         self.key_dir.insert(key, entry);
+
         Ok(())
     }
 
@@ -140,120 +159,127 @@ impl KvStore {
         if self.key_dir.get(&key).is_none() {
             return Err(KvsError::KeyNotFound);
         }
-        let entry = write_entry(self.writer.get_mut(), &key, &TOMBSTONE.to_string())?;
+        let entry = self.write_entry(&key, &TOMBSTONE.to_string())?;
         self.key_dir.insert(key, entry);
         Ok(())
+    }
+
+    // Write a key/value pair to the given writer in the bitcask format.
+    // An entry indicating the location of the value for the given key is returned.
+    // Fixed-width header            Variable-length body
+    //+=====+=====+=====+====== - - +============== - - +
+    //| u16 | u64 | u32 | u32       | [u8] | [u8] |
+    //+=====+=====+=====+====== - - +============== - - +
+    // checksum (2 bytes)
+    // timestamp (8 bytes)
+    // key_len (4 bytes)
+    // val_len (4 bytes)
+    // key (key_len bytes)
+    // value (val_len bytes)
+    fn write_entry(&mut self, key: &String, value: &String) -> Result<Entry> {
+        let key_len = key.len();
+        let value_len = value.len();
+        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+        let mut entry = Vec::<u8>::with_capacity(8 + 4 + 4 + key_len + value_len);
+
+        entry.write_u64::<BigEndian>(timestamp)?;
+        entry.write_u32::<BigEndian>(key_len as u32)?;
+        entry.write_u32::<BigEndian>(value_len as u32)?;
+        entry.write_all(key.as_bytes())?;
+        entry.write_all(value.as_bytes())?;
+
+        let checksum = X25.checksum(&entry);
+
+        self.writer.write_u16::<BigEndian>(checksum)?;
+        self.writer.write_all(&entry)?;
+        self.writer.flush()?;
+
+        let value_pos = self.writer.seek(std::io::SeekFrom::Current(0))? - value_len as u64;
+
+        Ok(Entry {
+            file_id: self.active_file_id,
+            value_len: value_len as u32,
+            value_pos,
+            timestamp,
+        })
+    }
+
+    // Read the next key/value entry from the given reader in the bitcask format.
+    // Fixed-width header            Variable-length body
+    //+=====+=====+=====+====== - - +============== - - +
+    //| u16 | u64 | u32 | u32       | [u8] | [u8] |
+    //+=====+=====+=====+====== - - +============== - - +
+    // checksum (2 bytes)
+    // timestamp (8 bytes)
+    // key_len (4 bytes)
+    // val_len (4 bytes)
+    // key (key_len bytes)
+    // value (val_len bytes)
+    fn read_next_entry<R: Read + Seek>(
+        reader: &mut R,
+        file_id: u64,
+    ) -> Result<Option<(String, Entry)>> {
+        // Check if we are at the end of the reader
+        // Move back to the current position after checking
+        let current_pos = reader.seek(std::io::SeekFrom::Current(0))?;
+        if current_pos == reader.seek(std::io::SeekFrom::End(0))? {
+            return Ok(None);
+        }
+        reader.seek(std::io::SeekFrom::Start(current_pos))?;
+
+        let checksum = reader.read_u16::<BigEndian>()?;
+        let timestamp = reader.read_u64::<BigEndian>()?;
+        let key_len = reader.read_u32::<BigEndian>()?;
+        let value_len = reader.read_u32::<BigEndian>()?;
+
+        let mut key_bytes = vec![0; key_len as usize];
+        reader.read_exact(&mut key_bytes)?;
+
+        let value_pos = reader.seek(std::io::SeekFrom::Current(0))?;
+
+        let mut value_bytes = vec![0; value_len as usize];
+        reader.read_exact(&mut value_bytes)?;
+
+        let mut entry_bytes =
+            Vec::<u8>::with_capacity(8 + 4 + 4 + key_len as usize + value_len as usize);
+        entry_bytes.write_u64::<BigEndian>(timestamp)?;
+        entry_bytes.write_u32::<BigEndian>(key_len)?;
+        entry_bytes.write_u32::<BigEndian>(value_len)?;
+        entry_bytes.write_all(&key_bytes)?;
+        entry_bytes.write_all(&value_bytes)?;
+
+        let read_checksum = X25.checksum(&entry_bytes);
+
+        if checksum != read_checksum {
+            return Err(KvsError::DataCorruption(checksum, read_checksum));
+        }
+
+        let entry = Entry {
+            file_id,
+            value_len,
+            value_pos,
+            timestamp,
+        };
+
+        let key = String::from_utf8(key_bytes)?;
+
+        Ok(Some((key, entry)))
+    }
+
+    fn read_value(&mut self, entry: &Entry) -> Result<Option<String>> {
+        if let Some(reader) = self.readers.get_mut(&entry.file_id) {
+            reader.seek(std::io::SeekFrom::Start(entry.value_pos))?;
+
+            let mut value_bytes = vec![0; entry.value_len as usize];
+            reader.read_exact(&mut value_bytes)?;
+
+            Ok(Some(String::from_utf8(value_bytes)?))
+        } else {
+            Ok(None)
+        }
     }
 }
 
 fn log_path(path: &Path, gen: u64) -> PathBuf {
     path.join(format!("{}.log", gen))
-}
-
-// Write a key/value pair to the given writer in the bitcask format.
-// An entry indicating the location of the value for the given key is returned.
-// Fixed-width header            Variable-length body
-//+=====+=====+=====+====== - - +============== - - +
-//| u16 | u64 | u32 | u32       | [u8] | [u8] |
-//+=====+=====+=====+====== - - +============== - - +
-// checksum (2 bytes)
-// timestamp (8 bytes)
-// key_len (4 bytes)
-// val_len (4 bytes)
-// key (key_len bytes)
-// value (val_len bytes)
-fn write_entry<W: Write + Seek>(writer: &mut W, key: &String, value: &String) -> Result<Entry> {
-    let key_len = key.len();
-    let value_len = value.len();
-    let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
-    let mut entry = Vec::<u8>::with_capacity(8 + 4 + 4 + key_len + value_len);
-
-    entry.write_u64::<BigEndian>(timestamp)?;
-    entry.write_u32::<BigEndian>(key_len as u32)?;
-    entry.write_u32::<BigEndian>(value_len as u32)?;
-    entry.write_all(key.as_bytes())?;
-    entry.write_all(value.as_bytes())?;
-
-    let checksum = X25.checksum(&entry);
-
-    writer.write_u16::<BigEndian>(checksum)?;
-    writer.write_all(&entry)?;
-    writer.flush()?;
-
-    let value_pos = writer.seek(std::io::SeekFrom::Current(0))? - value_len as u64;
-
-    Ok(Entry {
-        file_id: 0,
-        value_len: value_len as u32,
-        value_pos,
-        timestamp,
-    })
-}
-
-// Read the next key/value entry from the given reader in the bitcask format.
-// Fixed-width header            Variable-length body
-//+=====+=====+=====+====== - - +============== - - +
-//| u16 | u64 | u32 | u32       | [u8] | [u8] |
-//+=====+=====+=====+====== - - +============== - - +
-// checksum (2 bytes)
-// timestamp (8 bytes)
-// key_len (4 bytes)
-// val_len (4 bytes)
-// key (key_len bytes)
-// value (val_len bytes)
-fn read_next_entry<R: Read + Seek>(reader: &mut R) -> Result<Option<(String, Entry)>> {
-    // Check if we are at the end of the reader
-    // Move back to the current position after checking
-    let current_pos = reader.seek(std::io::SeekFrom::Current(0))?;
-    if current_pos == reader.seek(std::io::SeekFrom::End(0))? {
-        return Ok(None);
-    }
-    reader.seek(std::io::SeekFrom::Start(current_pos))?;
-
-    let checksum = reader.read_u16::<BigEndian>()?;
-    let timestamp = reader.read_u64::<BigEndian>()?;
-    let key_len = reader.read_u32::<BigEndian>()?;
-    let value_len = reader.read_u32::<BigEndian>()?;
-
-    let mut key_bytes = vec![0; key_len as usize];
-    reader.read_exact(&mut key_bytes)?;
-
-    let value_pos = reader.seek(std::io::SeekFrom::Current(0))?;
-
-    let mut value_bytes = vec![0; value_len as usize];
-    reader.read_exact(&mut value_bytes)?;
-
-    let mut entry_bytes =
-        Vec::<u8>::with_capacity(8 + 4 + 4 + key_len as usize + value_len as usize);
-    entry_bytes.write_u64::<BigEndian>(timestamp)?;
-    entry_bytes.write_u32::<BigEndian>(key_len)?;
-    entry_bytes.write_u32::<BigEndian>(value_len)?;
-    entry_bytes.write_all(&key_bytes)?;
-    entry_bytes.write_all(&value_bytes)?;
-
-    let read_checksum = X25.checksum(&entry_bytes);
-
-    if checksum != read_checksum {
-        return Err(KvsError::DataCorruption(checksum, read_checksum));
-    }
-
-    let entry = Entry {
-        file_id: 0,
-        value_len,
-        value_pos,
-        timestamp,
-    };
-
-    let key = String::from_utf8(key_bytes)?;
-
-    Ok(Some((key, entry)))
-}
-
-fn read_value<R: Read + Seek>(reader: &mut R, entry: &Entry) -> Result<String> {
-    reader.seek(std::io::SeekFrom::Start(entry.value_pos))?;
-
-    let mut value_bytes = vec![0; entry.value_len as usize];
-    reader.read_exact(&mut value_bytes)?;
-
-    Ok(String::from_utf8(value_bytes)?)
 }
