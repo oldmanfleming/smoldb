a70b1e167b0a6713cee6b552ba0c7dcb0115c8fc
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-02-20 14:58:08 -0800

    Add failing integration tests for client/server

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,177 +1,337 @@
 use assert_cmd::prelude::*;
-use predicates::ord::eq;
-use predicates::str::{contains, is_empty, PredicateStrExt};
-use smoldb::{Result, Storage};
+use predicates::str::{contains, is_empty};
+use std::fs::{self, File};
 use std::process::Command;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use tempfile::TempDir;
 
-// `smoldb` with no args should exit with a non-zero code.
+// `smolcli` with no args should exit with a non-zero code.
 #[test]
-fn cli_no_args() {
-    Command::cargo_bin("smoldb").unwrap().assert().failure();
+fn client_cli_no_args() {
+    let temp_dir = TempDir::new().unwrap();
+    let mut cmd = Command::cargo_bin("smolcli").unwrap();
+    cmd.current_dir(&temp_dir).assert().failure();
 }
 
-// `smoldb -V` should print the version
 #[test]
-fn cli_version() {
-    Command::cargo_bin("smoldb")
+fn client_cli_invalid_get() {
+    let temp_dir = TempDir::new().unwrap();
+    Command::cargo_bin("smolcli")
         .unwrap()
-        .args(&["-V"])
+        .args(&["get"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "extra", "field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+#[test]
+fn client_cli_invalid_set() {
+    let temp_dir = TempDir::new().unwrap();
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["set"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["set", "missing_field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["set", "key", "value", "extra_field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["set", "key", "value", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+#[test]
+fn client_cli_invalid_rm() {
+    let temp_dir = TempDir::new().unwrap();
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["rm"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["rm", "extra", "field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["rm", "key", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["rm", "key", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+#[test]
+fn client_cli_invalid_subcommand() {
+    let temp_dir = TempDir::new().unwrap();
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["unknown"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+// `smolcli -V` should print the version
+#[test]
+fn client_cli_version() {
+    let temp_dir = TempDir::new().unwrap();
+    let mut cmd = Command::cargo_bin("smolcli").unwrap();
+    cmd.args(&["-V"])
+        .current_dir(&temp_dir)
         .assert()
         .stdout(contains(env!("CARGO_PKG_VERSION")));
 }
 
-// `smoldb get <KEY>` should print "Key not found" for a non-existent key and exit with zero.
+// `smoldb -V` should print the version
 #[test]
-fn cli_get_non_existent_key() {
+fn server_cli_version() {
     let temp_dir = TempDir::new().unwrap();
-    Command::cargo_bin("smoldb")
+    let mut cmd = Command::cargo_bin("smoldb").unwrap();
+    cmd.args(&["-V"])
+        .current_dir(&temp_dir)
+        .assert()
+        .stdout(contains(env!("CARGO_PKG_VERSION")));
+}
+
+#[test]
+fn cli_log_configuration() {
+    let temp_dir = TempDir::new().unwrap();
+    let stderr_path = temp_dir.path().join("stderr");
+    let mut cmd = Command::cargo_bin("smoldb").unwrap();
+    let mut child = cmd
+        .args(&["--engine", "kvs", "--addr", "127.0.0.1:4001"])
+        .current_dir(&temp_dir)
+        .stderr(File::create(&stderr_path).unwrap())
+        .spawn()
+        .unwrap();
+    thread::sleep(Duration::from_secs(1));
+    child.kill().expect("server exited before killed");
+
+    let content = fs::read_to_string(&stderr_path).expect("unable to read from stderr file");
+    assert!(content.contains(env!("CARGO_PKG_VERSION")));
+    assert!(content.contains("kvs"));
+    assert!(content.contains("127.0.0.1:4001"));
+}
+
+#[test]
+fn cli_wrong_engine() {
+    // sled first, kvs second
+    {
+        let temp_dir = TempDir::new().unwrap();
+        let mut cmd = Command::cargo_bin("smoldb").unwrap();
+        let mut child = cmd
+            .args(&["--engine", "sled", "--addr", "127.0.0.1:4002"])
+            .current_dir(&temp_dir)
+            .spawn()
+            .unwrap();
+        thread::sleep(Duration::from_secs(1));
+        child.kill().expect("server exited before killed");
+
+        let mut cmd = Command::cargo_bin("smoldb").unwrap();
+        cmd.args(&["--engine", "kvs", "--addr", "127.0.0.1:4003"])
+            .current_dir(&temp_dir)
+            .assert()
+            .failure();
+    }
+
+    // kvs first, sled second
+    {
+        let temp_dir = TempDir::new().unwrap();
+        let mut cmd = Command::cargo_bin("smoldb").unwrap();
+        let mut child = cmd
+            .args(&["--engine", "kvs", "--addr", "127.0.0.1:4002"])
+            .current_dir(&temp_dir)
+            .spawn()
+            .unwrap();
+        thread::sleep(Duration::from_secs(1));
+        child.kill().expect("server exited before killed");
+
+        let mut cmd = Command::cargo_bin("smoldb").unwrap();
+        cmd.args(&["--engine", "sled", "--addr", "127.0.0.1:4003"])
+            .current_dir(&temp_dir)
+            .assert()
+            .failure();
+    }
+}
+
+fn cli_access_server(engine: &str, addr: &str) {
+    let (sender, receiver) = mpsc::sync_channel(0);
+    let temp_dir = TempDir::new().unwrap();
+    let mut server = Command::cargo_bin("smoldb").unwrap();
+    let mut child = server
+        .args(&["--engine", engine, "--addr", addr])
+        .current_dir(&temp_dir)
+        .spawn()
+        .unwrap();
+    let handle = thread::spawn(move || {
+        let _ = receiver.recv(); // wait for main thread to finish
+        child.kill().expect("server exited before killed");
+    });
+    thread::sleep(Duration::from_secs(1));
+
+    Command::cargo_bin("smolcli")
         .unwrap()
-        .args(&["get", "key1"])
+        .args(&["set", "key1", "value1", "--addr", addr])
         .current_dir(&temp_dir)
         .assert()
         .success()
-        .stdout(eq("Key not found").trim());
-}
+        .stdout(is_empty());
 
-// `smoldb rm <KEY>` should print "Key not found" for an empty database and exit with non-zero code.
-#[test]
-fn cli_rm_non_existent_key() {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    Command::cargo_bin("smoldb")
+    Command::cargo_bin("smolcli")
         .unwrap()
-        .args(&["rm", "key1"])
+        .args(&["get", "key1", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout("value1\n");
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["set", "key1", "value2", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout(is_empty());
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key1", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout("value2\n");
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key2", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout(contains("Key not found"));
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["rm", "key2", "--addr", addr])
         .current_dir(&temp_dir)
         .assert()
         .failure()
-        .stdout(eq("Key not found").trim());
-}
+        .stderr(contains("Key not found"));
 
-// `smoldb set <KEY> <VALUE>` should print nothing and exit with zero.
-#[test]
-fn cli_set() {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-    Command::cargo_bin("smoldb")
+    Command::cargo_bin("smolcli")
         .unwrap()
-        .args(&["set", "key1", "value1"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success()
-        .stdout(is_empty());
-}
-
-#[test]
-fn cli_get_stored() -> Result<()> {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-
-    let mut store = Storage::open(temp_dir.path())?;
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    store.set("key2".to_owned(), "value2".to_owned())?;
-    drop(store);
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["get", "key1"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success()
-        .stdout(eq("value1").trim());
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["get", "key2"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success()
-        .stdout(eq("value2").trim());
-
-    Ok(())
-}
-
-// `smoldb rm <KEY>` should print nothing and exit with zero.
-#[test]
-fn cli_rm_stored() -> Result<()> {
-    let temp_dir = TempDir::new().expect("unable to create temporary working directory");
-
-    let mut store = Storage::open(temp_dir.path())?;
-    store.set("key1".to_owned(), "value1".to_owned())?;
-    drop(store);
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["rm", "key1"])
+        .args(&["set", "key2", "value3", "--addr", addr])
         .current_dir(&temp_dir)
         .assert()
         .success()
         .stdout(is_empty());
 
-    Command::cargo_bin("smoldb")
+    Command::cargo_bin("smolcli")
         .unwrap()
-        .args(&["get", "key1"])
+        .args(&["rm", "key1", "--addr", addr])
         .current_dir(&temp_dir)
         .assert()
         .success()
-        .stdout(eq("Key not found").trim());
+        .stdout(is_empty());
 
-    Ok(())
+    sender.send(()).unwrap();
+    handle.join().unwrap();
+
+    // Reopen and check value
+    let (sender, receiver) = mpsc::sync_channel(0);
+    let mut server = Command::cargo_bin("smoldb").unwrap();
+    let mut child = server
+        .args(&["--engine", engine, "--addr", addr])
+        .current_dir(&temp_dir)
+        .spawn()
+        .unwrap();
+    let handle = thread::spawn(move || {
+        let _ = receiver.recv(); // wait for main thread to finish
+        child.kill().expect("server exited before killed");
+    });
+    thread::sleep(Duration::from_secs(1));
+
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key2", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout(contains("value3"));
+    Command::cargo_bin("smolcli")
+        .unwrap()
+        .args(&["get", "key1", "--addr", addr])
+        .current_dir(&temp_dir)
+        .assert()
+        .success()
+        .stdout(contains("Key not found"));
+    sender.send(()).unwrap();
+    handle.join().unwrap();
 }
 
 #[test]
-fn cli_invalid_get() {
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["get"])
-        .assert()
-        .failure();
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["get", "extra", "field"])
-        .assert()
-        .failure();
+fn cli_access_server_kvs_engine() {
+    cli_access_server("kvs", "127.0.0.1:4004");
 }
 
 #[test]
-fn cli_invalid_set() {
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["set"])
-        .assert()
-        .failure();
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["set", "missing_field"])
-        .assert()
-        .failure();
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["set", "extra", "extra", "field"])
-        .assert()
-        .failure();
-}
-
-#[test]
-fn cli_invalid_rm() {
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["rm"])
-        .assert()
-        .failure();
-
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["rm", "extra", "field"])
-        .assert()
-        .failure();
-}
-
-#[test]
-fn cli_invalid_subcommand() {
-    Command::cargo_bin("smoldb")
-        .unwrap()
-        .args(&["unknown", "subcommand"])
-        .assert()
-        .failure();
+fn cli_access_server_sled_engine() {
+    cli_access_server("sled", "127.0.0.1:4005");
 }
