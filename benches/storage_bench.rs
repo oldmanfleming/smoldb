ba8ee2a2c1182470b1afcc4572373a16718ff2b8
Evan Fleming <evan.gordon.fleming@gmail.com>
2024-02-25 17:26:04 -0800

    Add benchmark for storage engines

---
+++ benches/storage_bench.rs
@@ -0,0 +1,76 @@
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use rand::prelude::*;
+use smoldb::{Bitcask, Sled, Storage};
+use tempfile::TempDir;
+
+fn set_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("set_bench");
+    group.bench_function("bitcask", |b| {
+        b.iter_batched(
+            || {
+                let temp_dir = TempDir::new().unwrap();
+                (Bitcask::open(temp_dir.path()).unwrap(), temp_dir)
+            },
+            |(mut store, _temp_dir)| {
+                for i in 1..(1 << 12) {
+                    store.set(format!("key{}", i), "value".to_string()).unwrap();
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+    group.bench_function("sled", |b| {
+        b.iter_batched(
+            || {
+                let temp_dir = TempDir::new().unwrap();
+                (Sled::open(temp_dir.path()).unwrap(), temp_dir)
+            },
+            |(mut db, _temp_dir)| {
+                for i in 1..(1 << 12) {
+                    db.set(format!("key{}", i), "value".to_string()).unwrap();
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+    group.finish();
+}
+
+fn get_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("get_bench");
+    for i in &vec![8, 12, 16, 20] {
+        group.bench_with_input(format!("bitcask_{}", i), i, |b, i| {
+            let temp_dir = TempDir::new().unwrap();
+            let mut store = Bitcask::open(temp_dir.path()).unwrap();
+            for key_i in 1..(1 << i) {
+                store
+                    .set(format!("key{}", key_i), "value".to_string())
+                    .unwrap();
+            }
+            let mut rng = SmallRng::from_seed([0; 32]);
+            b.iter(|| {
+                store
+                    .get(format!("key{}", rng.gen_range(1..1 << i)))
+                    .unwrap();
+            })
+        });
+    }
+    for i in &vec![8, 12, 16, 20] {
+        group.bench_with_input(format!("sled_{}", i), i, |b, i| {
+            let temp_dir = TempDir::new().unwrap();
+            let mut db = Sled::open(temp_dir.path()).unwrap();
+            for key_i in 1..(1 << i) {
+                db.set(format!("key{}", key_i), "value".to_string())
+                    .unwrap();
+            }
+            let mut rng = SmallRng::from_seed([0; 32]);
+            b.iter(|| {
+                db.get(format!("key{}", rng.gen_range(1..1 << i))).unwrap();
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, set_bench, get_bench);
+criterion_main!(benches);
